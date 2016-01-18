ded3cd144e2b637d754dfe543388de3d6ddab692
NavyaEV <NavyaEV@missionrnd.com>
2016-01-18 20:28:19 +0530

    Submit Commit

--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -20,10 +20,83 @@ struct transaction {
 	char date[11];
 	char description[20];
 };
-
+void finddate1(char *, int *);
+int getvalue1(char *, int, int);
+int check1(int *, int *);
 struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	
+	int i=0, j=0, k=-1, key,d1[3],d2[3];
+	struct transaction merge[10];
 	if ((A==nullptr)||(B==nullptr))
 		return NULL;
-	
+	while ((i<ALen)&&(j<BLen))
+	{
+		finddate1(A[i].date, d1);
+		finddate1(B[j].date, d2);
+		key = check1(d1, d2);
+		if (key==-1)
+		{
+			merge[++k] = A[i];
+			i++;
+		}
+		else if (key==0)
+		{
+			merge[++k] = A[i];
+			merge[++k] = B[j];
+			i++;
+			j++;
+		}
+		else
+		{
+			merge[++k] = B[j];
+			j++;
+		}
+	}
+	if (i == ALen)
+	{
+		while (j < BLen)
+		{
+			merge[++k] = B[j];
+			j++;
+		}
+	}
+	else
+	{
+		while (i < ALen)
+		{
+			merge[++k] = A[i];
+			i++;
+		}
+	}
+	return merge;
+}
+int check1(int d1[], int d2[])
+{
+	if (d1[2] < d2[2])
+		return -1;
+	else if (d1[2] > d2[2])
+		return 1;
+	if (d1[1] < d2[1])
+		return -1;
+	else if (d1[1] > d2[1])
+		return 1;
+	if (d1[0] < d2[0])
+		return -1;
+	else if (d1[0] > d2[0])
+		return 1;
+	return 0;
+}
+void finddate1(char *date, int d[])
+{
+	d[0] = getvalue1(date, 0, 1);
+	d[1] = getvalue1(date, 3, 4);
+	d[2] = getvalue1(date, 6, 9);
+}
+int getvalue1(char *date, int x, int y)
+{
+	int i, num = 0;
+	for (i = x; i <= y; i++)
+	{
+		num = (num * 10) + (date[i] - 48);
+	}
+	return num;
 }
--- src/sortedArraysCommonElements.cpp
+++ src/sortedArraysCommonElements.cpp
@@ -21,7 +21,66 @@ struct transaction {
 	char date[11];
 	char description[20];
 };
-
+void finddate2(char *, int *);
+int getvalue2(char *, int, int);
+int check2(int *, int *);
 struct transaction * sortedArraysCommonElements(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+	int i = 0, j = 0, k = -1, key, d1[3], d2[3];
+	struct transaction common[10];
+	if ((A == nullptr) || (B==nullptr))
+		return NULL;
+	while ((i<ALen) && (j<BLen))
+	{
+		finddate2(A[i].date, d1);
+		finddate2(B[j].date, d2);
+		key = check2(d1, d2);
+		if (key == -1)
+		{
+			i++;
+		}
+		else if (key == 0)
+		{
+			common[++k] = A[i];
+			i++;
+			j++;
+		}
+		else
+		{
+			j++;
+		}
+	}
+	if (k == -1)
+		return NULL;
+	return common;
+}
+int check2(int d1[], int d2[])
+{
+	if (d1[2] < d2[2])
+		return -1;
+	else if (d1[2] > d2[2])
+		return 1;
+	if (d1[1] < d2[1])
+		return -1;
+	else if (d1[1] > d2[1])
+		return 1;
+	if (d1[0] < d2[0])
+		return -1;
+	else if (d1[0] > d2[0])
+		return 1;
+	return 0;
+}
+void finddate2(char *date, int d[])
+{
+	d[0] = getvalue2(date, 0, 1);
+	d[1] = getvalue2(date, 3, 4);
+	d[2] = getvalue2(date, 6, 9);
+}
+int getvalue2(char *date, int x, int y)
+{
+	int i, num = 0;
+	for (i = x; i <= y; i++)
+	{
+		num = (num * 10) + (date[i] - 48);
+	}
+	return num;
 }
